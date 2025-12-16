b6ee4bf36e8262f1cf3a2faf2e7fddbf693c8e07
Sam Lantinga <slouken@libsdl.org>
2025-12-16 00:35:07 -0800

    Maelstrom 4.0 is relicensed under the Zlib license

--- maclib/Mac_FontServ.h
+++ maclib/Mac_FontServ.h
@@ -1,6 +1,6 @@
 /*
   maclib:  A companion library to SDL for working with Macintosh (tm) data
-  Copyright (C) 1997-2011 Sam Lantinga <slouken@libsdl.org>
+  Copyright (C) 1997-2025 Sam Lantinga <slouken@libsdl.org>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
