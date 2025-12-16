10fd0eee2d01d9c1c9a1d4055739bf68e1caa4d8
Sam Lantinga <slouken@libsdl.org>
2025-12-16 00:35:07 -0800

    Maelstrom 4.0 is relicensed under the Zlib license

--- maclib/Mac_Resource.h
+++ maclib/Mac_Resource.h
@@ -1,30 +1,29 @@
 /*
-    MACLIB:  A companion library to SDL for working with Macintosh (tm) data
-    Copyright (C) 1997-2021 Sam Lantinga <slouken@libsdl.org>
+  maclib:  A companion library to SDL for working with Macintosh (tm) data
+  Copyright (C) 1997-2025 Sam Lantinga <slouken@libsdl.org>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
 */
-
 #ifndef _Mac_Resource_h
 #define _Mac_Resource_h
 
 /* These are routines to parse a Macintosh Resource Fork file
 
-	-Sam Lantinga			(slouken@devolution.com)
-
-Note: Most of the info in this file came from "Inside Macintosh"
+   Note: Most of the info in this file came from "Inside Macintosh"
 */
 
 #include <stdio.h>
