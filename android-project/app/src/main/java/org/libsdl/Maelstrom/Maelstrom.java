f4e89cf3fda6e68fd2f5898c5514d574acc25e2b
Sam Lantinga <slouken@libsdl.org>
2026-03-21 15:55:22 -0700

    Added Android build of Maelstrom

---
+++ android-project/app/src/main/java/org/libsdl/Maelstrom/Maelstrom.java
@@ -0,0 +1,9 @@
+package org.libsdl.Maelstrom;
+
+import org.libsdl.app.SDLActivity;
+
+/**
+ * A sample wrapper class that just calls SDLActivity
+ */
+
+public class Maelstrom extends SDLActivity { }
